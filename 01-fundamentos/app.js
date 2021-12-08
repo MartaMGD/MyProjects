258eaea4855078ce1cd1e9a66bd348c68263c051
Marta Gonzalez <martagd96@gmail.com>
2021-12-08 20:39:30 +0100

    .

--- 01-fundamentos/app.js
+++
@@ -1,8 +0,0 @@
-
-// console.log("Hola Mundo");
-
-let a = 10;
-var b = 10;
-const c = 10; 
-
-// La diferencia es que let y var se pueden modificar y una const no. 
---
+++ 01-fundamentos/js/primitivos.js
@@ -0,0 +1,19 @@
+let nombre = "Peter Parker";
+console.log(nombre);
+
+nombre = "Marta";
+console.log(nombre);
+
+nombre = "Tía May";
+nombre = `Tía May`;
+
+console.log(typeof nombre); // Type string
+
+nombre = 123; 
+console.log(typeof nombre); // Type number
+
+let esMarvel = true; 
+console.log(typeof esMarvel); // Type boolean
+
+let edad = 25;
+console.log(typeof edad); // Type number
