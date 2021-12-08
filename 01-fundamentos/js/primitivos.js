9421a04c89614c17a8b17f775d5ef51624f94932
Marta Gonzalez <martagd96@gmail.com>
2021-12-08 20:43:25 +0100

    .

--- 01-fundamentos/js/primitivos.js
+++ 01-fundamentos/js/primitivos.js
@@ -17,3 +17,12 @@ console.log(typeof esMarvel); // Type boolean
 
 let edad = 25;
 console.log(typeof edad); // Type number
+
+let superPoderDeSpiderMan; // Type undefined
+
+let soyNull = null; // Da objeto
+
+let symbol1 = Symbol("a"); // Tipo symbol
+let symbol2 = Symbol("a");
+
+console.log(symbol1 === symbol2); // false 
